ab38b954353ebf88dbd28a98e3013a99fdf2cab3
HubbleCommand <sasha.poirier@gmail.com>
2022-12-04 19:47:39 +0100

    main request storage permissions

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:gta_player/widgets/loading.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,6 +11,20 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Permission.storage.status.then((value) {
+      if (value != PermissionStatus.granted) {
+        Permission.storage.request().then((value){
+          Permission.manageExternalStorage.status.then((valueInner) {
+            if (valueInner != PermissionStatus.granted) {
+              Permission.manageExternalStorage.request().then((valueInner){
+                debugPrint("Storage : ${value} External storage : ${valueInner}");
+              });
+            }
+          });
+        });
+      }
+    });
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
