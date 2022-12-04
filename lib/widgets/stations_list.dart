7f4d0fff866ff75927f0a7e68b9e2e8fab1a354a
HubbleCommand <sasha.poirier@gmail.com>
2022-12-04 19:52:13 +0100

    station list fixed scrolling

--- lib/widgets/stations_list.dart
+++ lib/widgets/stations_list.dart
@@ -13,19 +13,21 @@ class StationsListWidget extends StatelessWidget {
         title: const Text("Select a Station"),
       ),
       body: SingleChildScrollView(
+        physics: const ClampingScrollPhysics(),
         child: ListView.builder(
-            scrollDirection: Axis.vertical,
-            shrinkWrap: true,
-            itemCount: stations.length,
-            itemBuilder: (context, index) {
-              return ListTile(
-                leading: Image.file(File(stations[index].icon), width: 50, height: 50,),
-                title: Text(stations[index].name),
-                onTap: () {
-                  Navigator.pop(context, index);
-                },
-              );
-            }
+          physics: const NeverScrollableScrollPhysics(),
+          scrollDirection: Axis.vertical,
+          shrinkWrap: true,
+          itemCount: stations.length,
+          itemBuilder: (context, index) {
+            return ListTile(
+              leading: Image.file(File(stations[index].icon), width: 50, height: 50,),
+              title: Text(stations[index].name, overflow: TextOverflow.ellipsis,),
+              onTap: () {
+                Navigator.pop(context, index);
+              },
+            );
+          }
           ),
         ),
     );
